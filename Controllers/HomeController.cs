3044e7c34b566696052b55ad25ef08cc84eadd5e
Chris Morton <CMorton@vitalresearch.com>
2020-09-15 08:06:29 -0700

    Update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using AD.Models;
+using AD.Domain.Models;
 
 namespace AD.Controllers
 {
